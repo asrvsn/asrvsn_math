2f493632eefa0e941cb9041e6badd9b30753e16c
Anand Srinivasan <anand@a0s.co>
2025-04-06 12:09:12 +0100

    updates

--- src/asrvsn_math/vectors.py
+++ src/asrvsn_math/vectors.py
@@ -5,14 +5,13 @@ Functions
 import numpy as np
 from typing import Tuple
 import pdb
+import numpy.linalg as la
 
 def vec_angle(v1: np.ndarray, v2: np.ndarray) -> float:
     '''
     Angle between two vectors.
     '''
-    v1_ = v1 / np.linalg.norm(v1)
-    v2_ = v2 / np.linalg.norm(v2)
-    return np.arccos(np.clip(np.dot(v1_, v2_), -1.0, 1.0))
+    return np.arccos(np.clip(cosine_similarity(v1, v2), -1.0, 1.0))
 
 def vec_angle_deg(v1: np.ndarray, v2: np.ndarray) -> float:
     '''
@@ -28,5 +27,10 @@ def middle_mode(v: np.ndarray, bins: int=100) -> Tuple[float, float]:
     hist, edges = hist[1:-1], edges[1:-1]
     l = (hist != 0).argmax()
     r = hist.size - (hist[::-1] != 0).argmax()
-    pdb.set_trace()
     return (edges[l], edges[r])
+
+def cosine_similarity(v1: np.ndarray, v2: np.ndarray) -> float:
+    return np.dot(v1, v2) / (la.norm(v1) * la.norm(v2))
+
+def angle_between_lines(v1: np.ndarray, v2: np.ndarray) -> float:
+    return np.arccos(abs(np.clip(cosine_similarity(v1, v2), -1.0, 1.0)))
